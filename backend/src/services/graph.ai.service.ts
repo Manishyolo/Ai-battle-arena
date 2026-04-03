8203b7153662573a0a7c3ce23f94e1a99461b386
Manish kumar <manish04kumarsingh@gmail.com>
2026-04-03 13:59:41 +0530

    feat: update package dependencies and enhance graph AI service with new state schema and judgement logic

--- backend/src/services/graph.ai.service.ts
+++ backend/src/services/graph.ai.service.ts
@@ -1,65 +1,66 @@
-import { HumanMessage } from "@langchain/core/messages";
-import {
-  StateSchema,
-  MessagesValue,
-  ReducedValue,
-  StateGraph,
-  START,
-  END,
-  Graph,
-} from "@langchain/langgraph";
-import type { GraphNode } from "@langchain/langgraph";
-import { Mistral_model,Gemini_model,Cohere_model } from "./model.service.js";
-import {z} from "zod";
+import { StateGraph,StateSchema,type GraphNode } from "@langchain/langgraph";
+import { Mistral_model,Cohere_model,Gemini_model } from "./model.service.js";
+import {createAgent , HumanMessage, providerStrategy} from "langchain";
 
-// state schema
-const State = new StateSchema({
-  messages: MessagesValue,
-  solution_1 : new ReducedValue(z.string().default(""),{
-    reducer:(current,next)=>{
-            return next
-    }
-  }),
-  solution_2: new ReducedValue(z.string().default(""),{
-    reducer:(current,next)=>{
-            return next
-    }
-  }),
-  judge_recommendation: new ReducedValue(z.object().default({
-     solution_1_score:0,
-     solution_2_score:0,
-     winner:"solution_1" as "solution_1" | "solution_2"
-  }),{
-    reducer:(current,next)=>{
-            return next
-      }})
+import z from "zod";
+
+
+const state = new StateSchema({
+    problem: z.string().default(""),
+    solution_1:z.string().default(""),
+    solution_2:z.string().default(""),
+    judgement:z.object({
+       solution_1_score:z.number().min(0).max(10).default(0),
+        solution_2_score:z.number().min(0).max(10).default(0),
+        solution_1_reasoning:z.string().default(""),
+        solution_2_reasoning:z.string().default(""),
+    })
 })
-// graph definition
-const solutionNode: GraphNode<typeof State> = async (state: typeof State) => {
 
-  const [mistralResponse, geminiResponse, cohereResponse] = await Promise.all([
-    Mistral_model.invoke(state.messages[0]),
-    Gemini_model.invoke(state.messages[0]),
-    Cohere_model.invoke(state.messages[0])
+const solutionNode:GraphNode<typeof state> = async (state)=>{
+        
+  const [cohere_response,mistral_response] = await Promise.all([
+    Cohere_model.invoke(state.problem),
+    Mistral_model.invoke(state.problem)
   ])
+     return {
+      solution_1: cohere_response.text,
+      solution_2: mistral_response.text
+     }
+}
 
-  console.log(state.messages);
-  return {
-    messages:state.messages[0]
-  }
-};
-const graph = new StateGraph(State)
-.addNode("solution", solutionNode)
-.addEdge(START,"solution")
-.compile();
+const judgementNode:GraphNode<typeof state> = async (state)=>{
+  const {solution_1,solution_2,problem} = state
+  const judge = createAgent({
+    model: Gemini_model,
+    responseFormat: providerStrategy(z.object({
+      solution_1_score:z.number().min(0).max(10),
+      solution_2_score:z.number().min(0).max(10),
+      solution_1_reasoning:z.string(),
+       solution_2_reasoning:z.string(),
+    })),
+    systemPrompt:"You are a judge tasked with evaluating two solutions genrated by different ai models. Please provide a score between 0 and 10 for each solution, along with your reasoning for the scores.",
+    
+ })
 
+ const judgeResponse = await judge.invoke({
+    messages:[
+      new HumanMessage(`
+           Problem: ${problem}
+            Solution 1: ${solution_1}
+            Solution 2: ${solution_2}
+            please evaluate the solutions and provide scores and reasoning
+        `)
+    ]
+ })
 
-export default async function(userMessage:string){
-     const result = await graph.invoke({
-         messages:[
-            new HumanMessage(userMessage)
-         ]
-     })
+ const {solution_1_score,solution_2_score,solution_1_reasoning,solution_2_reasoning} = judgeResponse.structuredResponse
+ 
+ return {
+  solution_1_score,
+  solution_2_score,
+  solution_1_reasoning,
+  solution_2_reasoning
+ }
 
-     return result.messages
 }
